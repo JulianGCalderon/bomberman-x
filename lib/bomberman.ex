1b9c13600673437f2605a1b454dae34392e40ca4
= <=>
2023-10-08 23:19:35 -0300

    detonate bomb (removes from board)

--- lib/bomberman.ex
+++ lib/bomberman.ex
@@ -1,2 +1,12 @@
 defmodule Bomberman do
+  def detonate(board, position) do
+    bomb = Board.at(board, position)
+
+    with %Element.Bomb{} <- bomb do
+      board = Board.put(board, position, :empty)
+      board
+    else
+      _ -> {:error, :no_bomb}
+    end
+  end
 end
--- test/bomberman_test.exs
+++ test/bomberman_test.exs
@@ -1,4 +1,18 @@
 defmodule BombermanTest do
   use ExUnit.Case
   doctest Bomberman
+
+  test "detonate removes bomb" do
+    board = Board.load!("boards/propagate_right.txt")
+
+    board = Bomberman.detonate(board, {0, 0})
+
+    assert :empty = Board.at(board, {0, 0})
+  end
+
+  test "detonate expects bomb" do
+    board = Board.load!("boards/propagate_right.txt")
+
+    assert {:error, :no_bomb} = Bomberman.detonate(board, {1, 0})
+  end
 end
